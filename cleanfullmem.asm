15eddd6cc56e357acd64d746bd72905be666ed35
Werner Ruotsalainen <>
2023-01-07 15:48:58 +0200

    a lot of failed (and at the end a working) attempts at using "SLEEP". Turns out macros MUST also be indented, otherwise dasm will complain about Unknown Mnemonic '<paramvalue like 0 with "SLEEP 0">'!

--- cleanfullmem.asm
+++ cleanfullmem.asm
@@ -12,6 +12,13 @@ Start:
 
     lda #12         
 	sta COLUBK
+	
+;	SLEEP 10000 ; error: segment: code fffc                    vs current org: 10396 - Aborting assembly - cleanfullmem.asm (27): error: Origin Reverse-indexed.
+;SLEEP ; no error?!?
+;SLEEP 0 ; cleanfullmem.asm (18): error: Unknown Mnemonic '0'.
+;SLEEP 1 ; error: Unknown Mnemonic '1'.
+;	SLEEP 1 ; with a prefixing indentation / tab, unlike before: 'MACRO ERROR: 'SLEEP': Duration must be > 1'
+	SLEEP 2 ; works!
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Fill  ROM size to exactly 4KB
