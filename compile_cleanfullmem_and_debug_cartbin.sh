53ba94819a79384548d5c5e72a4c4e2b79c7158a
Werner Ruotsalainen <>
2023-01-07 21:29:53 +0200

    Prolly it's the best to always start Stella in immediate executing mode and only switch to debug on demand with the keypress to check changes to color updates etc.

--- compile_cleanfullmem_and_debug_cartbin.sh
+++ compile_cleanfullmem_and_debug_cartbin.sh
@@ -1,5 +1,5 @@
 #bash
 if  dasm cleanfullmem.asm -f3 -v0 -ocart.bin
 then
-        /Applications/Stella.app/Contents/MacOS/Stella -debug cart.bin 
+        /Applications/Stella.app/Contents/MacOS/Stella  cart.bin #-debug
 fi
