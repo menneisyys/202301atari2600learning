17a26864d2342a5fcb207fb923f4b02d1db0d6c7
Werner Ruotsalainen <>
2023-01-09 23:29:39 +0200

    "5. Painting on the CRT": my First attempt at trying to reduce the "scrolling" (color cycling) speed: I introed a 1, new .byte with the value 3 (so that cycling is done with 1/3 speed - we decrease from 3 to 0 and only then decrease the generic color instead of in every single frame) and 2, the code to do the above decrease However, it doesn't work at all - absolutely no color cycling is visible! Surely b/c stuff like .byte is in a cart and NOT a ROM and we should use something like the "BGColor	equ $81", which keeps a variable in the RAM (at the very beginning: $81) - next, I'll implement that one for my countdown.

--- cleanfullmem.asm
+++ cleanfullmem.asm
@@ -20,6 +20,7 @@
 ; Let's define a variable to hold the starting color
 ; at memory address $81
 BGColor	equ $81
+globalVarForSlowerScrolling: .byte 3
 
 ; The CLEAN_START macro zeroes RAM and registers
 Start	CLEAN_START
@@ -71,6 +72,12 @@ LVOver	sta WSYNC
 ; The next frame will start with current color value - 1
 ; to get a downwards scrolling effect
 	dec BGColor
+        ;dec globalVarForSlowerScrolling
+        beq NextFrame
+        lda globalVarForSlowerScrolling
+        sta BGColor
+        lda #$03
+        sta globalVarForSlowerScrolling
 
 ; Go back and do another frame
 	jmp NextFrame
